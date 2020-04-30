dd4c574a176c5ce1d614b089e3aec6cd7676a91f
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2020-04-30 20:46:11 +0100

    add ex008

---
+++ Exercicios/ex008.py
@@ -0,0 +1,15 @@
+m = int(input('Digite uma medida: '))
+dm = m * 10
+cm = m * 100
+mm = m * 1000
+dam = m / 10
+hm = m / 100
+km = m / 1000
+print(f'{m} metros corresponde a ')
+print(f'{km} kilometros')
+print(f'{hm} hectometros')
+print(f'{dam} decametros')
+print(f'{dm} decimetros')
+print(f'{cm} centimetros')
+print(f'{mm} milimetros')
+
