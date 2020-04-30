1e2c07dcf6f4e64838d19882716f8976c8f9145e
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2020-04-30 19:45:28 +0100

    add ex006

---
+++ Exercicios/ex006.py
@@ -0,0 +1,8 @@
+import math
+n = int(input('Digite um número: '))
+dobro = n * 2
+triplo = n * 3
+raiz = math.sqrt(n)
+print(f'O dobro do número {n} é {dobro}')
+print(f'O triplo do número {n} é {triplo}')
+print(f'A raiz quadrada do número {n} é {raiz}')
