465085eddbba87e2982b7d263970e8ad45afe550
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2020-04-30 17:20:28 +0100

    update ex003

--- Exercicios/ex003.py
+++ Exercicios/ex003.py
@@ -1,4 +1,4 @@
 n1 = int(input('Digite um número: '))
 n2 = int(input('Digite outro: '))
-s = n1 + n2
-print(f'A soma entre {n1} e {n2} é {s}')
+soma = n1 + n2
+print(f'A soma entre {n1} e {n2} é {soma}')
