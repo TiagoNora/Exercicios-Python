754a35612cc1a0c90141e80893fe5a4eaec5d859
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2020-04-30 20:51:22 +0100

    add ex009

---
+++ Exercicios/ex009.py
@@ -0,0 +1,12 @@
+t = int(input('Digite um numero: '))
+print(f'{t} * 1 = {t*1}')
+print(f'{t} * 2 = {t*2}')
+print(f'{t} * 3 = {t*3}')
+print(f'{t} * 4 = {t*4}')
+print(f'{t} * 5 = {t*5}')
+print(f'{t} * 6 = {t*6}')
+print(f'{t} * 7 = {t*7}')
+print(f'{t} * 8 = {t*8}')
+print(f'{t} * 9 = {t*9}')
+print(f'{t} * 10 = {t*10}')
+
