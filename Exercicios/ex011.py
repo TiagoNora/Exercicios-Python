b114ccd614e62d2c7b46de922533568065b4b8c9
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2020-05-01 17:33:35 +0100

    add ex001

---
+++ Exercicios/ex011.py
@@ -0,0 +1,5 @@
+largura = float(input('Digite a largura da parede: '))
+comprimento = float(input('Digite o comprimento da parede: '))
+area = largura * comprimento
+litros = area / 2
+print(f'Para uma parede de {area} m2 é preciso {litros} litros.')
