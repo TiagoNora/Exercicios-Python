b6edee8f33fc23719febffa33ef798f9cd9e831c
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2020-05-01 17:45:29 +0100

    add ex012

---
+++ Exercicios/ex0012.py
@@ -0,0 +1,3 @@
+produto = float(input('Digite o valor do produto: '))
+precof = produto - (produto * 0.05)
+print(f'Um produto com valor de {produto} euros fica a {precof} euros ápos um desconto de 5%.')
