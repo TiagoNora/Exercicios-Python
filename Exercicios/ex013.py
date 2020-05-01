df6ef5bf37b45af7be38b205d3f8e089e3f69dae
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2020-05-01 17:58:05 +0100

    add ex013

---
+++ Exercicios/ex013.py
@@ -0,0 +1,3 @@
+salario = float(input('Digite o valor do salário: '))
+ajuste = salario + (salario * 0.15)
+print(f'Um funcionário com um salario de {salario} euros, após um aumento de 15% passa a receber {ajuste} euros ')
