b1bc1757771a39bfc5c1d874da23ed0075def9f6
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2020-05-02 18:31:30 +0100

    add ex015

---
+++ Exercicios/ex015.py
@@ -0,0 +1,4 @@
+km = float(input('Digite o número de km rodados: '))
+dias = int(input('Digite o número de dias alugado: '))
+valor = dias * 60 + km * 0.15
+print(f'O valor total a ser pago é {valor} euros.')
