348ed76ba5d90ebee656e115b9f7d4e4afeb012b
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2020-05-02 18:48:05 +0100

    update ex017

--- Exercicios/ex017.py
+++ Exercicios/ex017.py
@@ -1,6 +1,6 @@
-import math
+from math import sqrt
 co = float(input('Introduza o cateto oposto do triângulo: '))
 ca = float(input('Introduza o cateto adjacente do triângulo: '))
 hi = (co ** 2) + (ca ** 2)
-hf = math.sqrt(hi)
+hf = sqrt(hi)
 print(f'Para um cateto oposto de {co} e um cateto adjacente de {ca} a hipotenusa é de {hf:.2f}')
