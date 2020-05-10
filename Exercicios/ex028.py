540f4cd8633926f98db3d10a9f00b0cd849a542c
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2020-05-10 02:03:33 +0100

    update ex028

--- Exercicios/ex028.py
+++ Exercicios/ex028.py
@@ -1,7 +1,10 @@
-import random
+from random import randint
 jogador = int(input('Digite um numero entre 0 e 5: '))
-pc = random.randint(0, 5)
-if jogador == pc:
-    print('Ganhou!')
+pc = randint(0, 5)
+if 0 <= jogador <= 5:
+    if jogador == pc:
+        print('Ganhou!')
+    elif jogador != pc:
+        print('Perdeu!')
 else:
-    print('Perdeu')
+    print('Entrada Inválida!.Introduza um numero entre 0 e 5!')
