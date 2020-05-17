29cce6bc2f48008fe46aedf2e0a412a1b48e2245
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2020-05-17 18:07:08 +0100

    add ex032

---
+++ Exercicios/ex032.py
@@ -0,0 +1,18 @@
+import calendar
+import datetime
+ano = int(input('Qual o ano que quer analisar.Intruduza 0 para analisar o ano atual: '))
+if ano != 0:
+    anob = calendar.isleap(ano)
+    if anob is True:
+        print(f'O ano {ano} é bissexto')
+    else:
+        print(f'O ano {ano} não é bissexto')
+if ano == 0:
+    anoatual = datetime.datetime.now().year
+    anoatualb = calendar.isleap(anoatual)
+    if anoatualb is True:
+        print(f'O ano {anoatual} é bissexto')
+    else:
+        print(f'O ano {anoatual} não é bissexto')
+
+
