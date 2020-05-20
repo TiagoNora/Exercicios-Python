3cfb0155fb8de0b30d9a0e6869bb15f56b10be08
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2020-05-20 23:25:19 +0100

    add ex035

---
+++ Exercicios/ex035.py
@@ -0,0 +1,10 @@
+s1 = int(input('Digite  valor do primeiro segmento: '))
+s2 = int(input('Digite  valor do segundo segmento: '))
+s3 = int(input('Digite  valor do terceiro segmento: '))
+if s1 > 0 and s2 > 0 and s3 > 0:
+    if s1 < s2 + s3 and s2 < s1 + s3 and s3 < s1 + s2:
+        print('Com os três segmentos dado dá para formar um triangulo.')
+    else:
+        print('Com os três segmentos dado não dá para formar um triangulo.')
+else:
+    print('Tente outra vez.')
