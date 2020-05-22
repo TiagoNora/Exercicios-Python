81d5e46fc32a89367569a22ad52ac8166e279475
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2020-05-22 10:31:11 +0100

    update ex029

--- Exercicios/ex029.py
+++ Exercicios/ex029.py
@@ -1,3 +1,6 @@
+# Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80Km/h,
+# Mostre uma mensagem dizendo que ele foi multado.
+# A multa vai custar R$7,00 por cada Km acima do limite.
 velocidade = int(input('Qual é a velocidade do carro: '))
 if velocidade > 80:
     multa = velocidade - 80
