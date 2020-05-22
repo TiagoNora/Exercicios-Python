81fcc4e0435c08411cb2c7e1e8fe108f8ffc03c2
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2020-05-22 10:30:46 +0100

    update ex025

--- Exercicios/ex025.py
+++ Exercicios/ex025.py
@@ -1,2 +1,3 @@
+#Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.
 nome = str(input('Digite o seu nome: '))
 print(f'O nome {nome.title()} tem "Silva" no nome: {"SILVA" in nome.upper()}')
