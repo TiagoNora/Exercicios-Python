28e0866c1ae9f28293694a1c48ebb173c40d0c63
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2020-05-22 10:31:23 +0100

    update ex031

--- Exercicios/ex031.py
+++ Exercicios/ex031.py
@@ -1,3 +1,5 @@
+#Desenvolva um programa que pergunte a distância de uma viagem em Km.
+# Calcule o preço da passagem, cobrando R$0,50 por Km para viagens de até 200Km e R$0,45 parta viagens mais longas.
 km = int(input('Qual é a distancia da viagem: '))
 if km <= 200:
     valor = km * 0.5
