bdc4e0ab3e890b3b8dfc16ab1200872a31f32cd5
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2020-05-22 10:28:58 +0100

    update ex012

--- Exercicios/ex012.py
+++ Exercicios/ex012.py
@@ -1,3 +1,4 @@
+#Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
 produto = float(input('Digite o valor do produto: '))
 precof = produto - (produto * 0.05)
 print(f'Um produto com valor de {produto} euros fica a {precof} euros ápos um desconto de 5%.')
