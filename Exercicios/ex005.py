88ee754491219c7cc4d6bddcf09ef040b0038904
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2020-05-22 10:28:18 +0100

    update ex005

--- Exercicios/ex005.py
+++ Exercicios/ex005.py
@@ -1,3 +1,4 @@
+#Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.
 numero = int(input('Digite um número: '))
 antecessor = numero - 1
 sucessor = numero + 1
