0d0a40d65825d71528ab1ceb1a150531341626b1
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2020-05-22 10:28:46 +0100

    update ex010

--- Exercicios/ex010.py
+++ Exercicios/ex010.py
@@ -1,3 +1,4 @@
+#Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
 euro = float(input('Quanto dinheiro tem em euros: '))
 dolar = euro * 1.10
 print(f'{euro} euros são equivalente a {dolar} dolares.')
