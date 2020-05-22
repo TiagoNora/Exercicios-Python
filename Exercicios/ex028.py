b962bc3a7da29e5a063d0264cc91a7ae7197e590
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2020-05-22 10:31:05 +0100

    update ex028

--- Exercicios/ex028.py
+++ Exercicios/ex028.py
@@ -1,3 +1,7 @@
+#Escreva um programa que faça o computador "pensar" em um número inteiro entre 0 e 5 e peça para o usuário
+#tentar descobrir qual foi o número escolhido pelo computador.
+#O programa deverá escrever na tela se o usuário venceu ou perdeu.
+
 from random import randint
 jogador = int(input('Digite um numero entre 0 e 5: '))
 pc = randint(0, 5)
