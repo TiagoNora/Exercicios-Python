5b470ef45aba74cdce448f008fbf277e2a656868
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2020-05-22 10:30:39 +0100

    update ex024

--- Exercicios/ex024.py
+++ Exercicios/ex024.py
@@ -1,3 +1,4 @@
+#Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome "SANTO".
 cidade = str(input('Digite o nome da sua cidade: '))
 dividido = cidade.split()
 print(f'{cidade} tem "PORTO" no nome: {"PORTO" in dividido[0].upper()}')
