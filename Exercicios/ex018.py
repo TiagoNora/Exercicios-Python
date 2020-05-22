7151c3bfdaca2f5ab432b20a90c8291cb77604fa
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2020-05-22 10:29:34 +0100

    update ex018

--- Exercicios/ex018.py
+++ Exercicios/ex018.py
@@ -1,3 +1,4 @@
+# Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.
 import math
 angulo = float(input('Dgite o angulo que deseja: '))
 seno = math.sin(math.radians(angulo))
