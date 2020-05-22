4e71f0dfbce72bc78c656c62a612085b8ed37544
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2020-05-22 10:29:10 +0100

    update ex014

--- Exercicios/ex014.py
+++ Exercicios/ex014.py
@@ -1,3 +1,4 @@
+#Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit.
 t = float(input('Introduza a temperatura em ºC: '))
 f = 1.8 * t + 32
 print(f'{t}ºC é igual a {f}ºF')
