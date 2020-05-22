df0853c3444ef0fe86b8f4727a613c6a783136e4
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2020-05-22 10:31:00 +0100

    update ex027

--- Exercicios/ex027.py
+++ Exercicios/ex027.py
@@ -1,3 +1,4 @@
+#Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
 nome = str(input('Digite o seu nome completo: ')).split()
 print(f'O seu primeiro nome é {nome[0]}.')
 print(f'O seu último nome é {nome[-1]}.')
