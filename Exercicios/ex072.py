9d9b9316d15c54626a06732c63795366dfcac709
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2020-06-28 15:29:01 +0100

    update ex072

--- Exercicios/ex072.py
+++ Exercicios/ex072.py
@@ -1,4 +1,6 @@
 #Crie um programa que tenha uma dupla totalmente preenchida com uma contagem por extenso, de zero até vinte.
+#Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
+
 extenso = ('Zero', 'Um', 'Dois', 'Três', 'Quatro','Cinco','Seis','Sete','Oito','Nove','Dez',
            'Onze','Doze', 'Treze', 'Quatorze', 'Quinze', 'Dezasseis', 'Dezassete', 'Dezoito', 'Dezanove', 'Vinte')
 while True:
