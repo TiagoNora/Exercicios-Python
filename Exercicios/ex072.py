0b4217d4c4ef4dbc4ee75ac3afde2056f3084775
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2020-06-28 15:21:19 +0100

    add ex072

---
+++ Exercicios/ex072.py
@@ -0,0 +1,8 @@
+extenso = ('Zero', 'Um', 'Dois', 'Três', 'Quatro','Cinco','Seis','Sete','Oito','Nove','Dez',
+           'Onze','Doze', 'Treze', 'Quatorze', 'Quinze', 'Dezasseis', 'Dezassete', 'Dezoito', 'Dezanove', 'Vinte')
+while True:
+    n = int(input('Digite um numero entre 0 e 20: '))
+    if 0 <= n <= 20:
+        break
+    print('Tente outra vez. ',end='')
+print(f'0 numero {n} em extenso lê-se {extenso[n]}.')
