4e8b7e4345294ba98d4d8d1984c6f11e29abff41
Tiago Nora <33982771+TiagoNora@users.noreply.github.com>
2020-07-27 13:20:36 +0100

    add ex099

---
+++ Exercicios/ex099.py
@@ -0,0 +1,21 @@
+#Faça um programa que tenha uma função chamada maior(), que receba vários parâmetros com valores inteiros.
+# Seu programa tem que analisar todos os valores e dizer qual deles é o maior.
+
+
+def maior(* num):
+    cont = maior_valor = 0
+    print('-=' * 30)
+    print('Processamento dos dados')
+    for n in num:
+        print(n, end=' ')
+        if cont == 0:
+            maior_valor = n
+        else:
+            if n > maior_valor:
+                maior_valor = n
+        cont += 1
+    print(f'Foram apresentados {cont} valores')
+    print(f'O maior número apresentado foi o {maior_valor}')
+
+
+maior(1, 2, 3)
